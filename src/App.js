98327db239ef1227a0d4e00d63176fea11db220d
lipan1 <2125524263@qq.com>
2019-07-11 11:17:25 +0800

    第一次提交

--- src/App.js
+++ src/App.js
@@ -2,14 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function sayhi(){
+  alert("hi");
+}
 function App() {
   return (
+    //XML =》JS
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={sayhi}>点我</button>
         <a
           className="App-link"
           href="https://reactjs.org"
