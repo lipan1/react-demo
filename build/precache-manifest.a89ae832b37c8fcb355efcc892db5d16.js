d802e624cf46e8df04ce0056e52fc9c72ac36e3b
lipan1 <2125524263@qq.com>
2019-07-11 11:44:22 +0800

    提交build

---
+++ build/precache-manifest.a89ae832b37c8fcb355efcc892db5d16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc134277cafd3f17d19926275f7a3b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d6a88115656d9b3d378",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "5d6a88115656d9b3d378",
+    "url": "/static/js/main.5f9ea287.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
